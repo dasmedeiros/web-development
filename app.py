23c7f2012aad3acc0b3d63ddcc96030bb9af9103
dasmedeiros <danielaugusto_10@hotmail.com>
2022-11-09 20:12:08 -0500

    First commit!

---
+++ app.py
@@ -0,0 +1,57 @@
+import datetime
+import os
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.getenv("DATABASE_URL"))
+    app.db = client.portfolio
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            #implement search engine later
+            return render_template("home.html")
+        return render_template("home.html")
+    
+    @app.route("/about/")
+    def about():
+        return render_template("about.html")
+
+    @app.route("/feedback/", methods=["GET", "POST"])
+    def feedback():
+        if request.method == "POST":
+            review_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": review_content, "date": formatted_date})
+        
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
+
+        return render_template("feedback.html", entries=entries_with_date)
+    
+    @app.route("/contact/", methods=["GET", "POST"])
+    def contact():
+        contact_name = ""
+        success = False
+        if request.method == "POST":
+            contact_name = request.form.get("name")
+            contact_email = request.form.get("email")
+            contact_tel = request.form.get("tel")
+            contact_message = request.form.get("message")
+            app.db.messages.insert_one({"name": contact_name, "email": contact_email, "tel": contact_tel, "message": contact_message})
+            success = True
+
+        return render_template("contact.html", contact_name=contact_name, success=success)    
+
+    return app
